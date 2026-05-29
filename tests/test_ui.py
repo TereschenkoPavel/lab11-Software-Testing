eb357ea3a93f2e53605b1a1702fb7b52d3e3ce12
Pavel Tereschenko <PATereschenko@gmail.com>
2026-05-29 15:35:17 +0300

    working version 1

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -9,14 +9,13 @@ import os
 @pytest.fixture
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Обязательно для CI
+    chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
     
-    # Путь к локальному файлу
     path = os.path.abspath("index.html")
     driver.get(f"file://{path}")
     yield driver
